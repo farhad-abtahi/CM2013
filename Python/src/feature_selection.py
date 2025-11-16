8958cd8df268565bd4fed15a1ea061fe5a8058b4
Sherry-yao-92 <sherry3431285@gmail.com>
2025-11-16 12:17:43 +0100

    Update feature_selection.py

--- Python/src/feature_selection.py
+++ Python/src/feature_selection.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+from sklearn.feature_selection import VarianceThreshold, f_classif, mutual_info_classif, SelectKBest
+from sklearn.preprocessing import MinMaxScaler
 
 def select_features(features, labels, config):
     """
@@ -32,11 +35,42 @@ def select_features(features, labels, config):
     if features.shape[1] == 0:
         print("⚠️  WARNING: No features to select from!")
         return features
-
-    if config.CURRENT_ITERATION <= 2:
+    
+    if config.CURRENT_ITERATION == 1:
         # Early iterations: Use all available features
         print("Early iteration - using all available features")
         selected_features = features
+
+    if config.CURRENT_ITERATION == 2:
+        # Normalize variance
+        scaler=MinMaxScaler()
+        var_scaled= scaler.fit_transform(features)
+        # Variance Thresholding
+        selected_var=VarianceThreshold(threshold=0.01)
+        selected_var.fit(var_scaled)
+        # Select features and send the original variance value to the next step
+        mask_var=selected_var.get_support()
+        var_selected_features=features[:,mask_var]
+
+        #Correlation Analysis
+        #Create a DataFrame
+        data=pd.DataFrame(var_selected_features)
+
+        #Correlation matrix
+        corr_matrix=data.corr()
+
+        #Select the upper bounds of the matrix
+        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+
+        #Select the columns that have a correlation bigger than a certain threshhold with any other column
+        to_drop = [column for column in upper.columns if any(upper[column] > 0.95)]
+        corr_selected_features=data.drop(to_drop, axis=1)
+
+        #Statistical Testing (Option B:Mutual Information)
+        MI_selected_features=SelectKBest(mutual_info_classif,k=40).fit_transform(corr_selected_features, labels)
+
+        print("Select best 40 features")
+        selected_features = MI_selected_features
 
     elif config.CURRENT_ITERATION == 3:
         # TODO: Students should implement feature selection here
